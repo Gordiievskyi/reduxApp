9c2e8b5d85ee5a6c0a3c2520507b4622fb07b22c
Viacheslav <sgordievsky@gmail.com>
2018-02-17 20:54:04 +0200

    Section 2 Lecture 17 completed

--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -3,12 +3,13 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {getBooks} from '../../actions/booksActions';
-class BookList extends React.Component {
-    componentDidMount(){
+import {Grid, Col, Row, Button} from 'react-bootstrap'
+        class BookList extends React.Component {
+    componentDidMount() {
         //Dispatch an action
         this.props.getBooks();
     }
-    
+
     render() {
         const booksList = this.props.books.map(function (booksArr) {
             return (
@@ -16,23 +17,26 @@ class BookList extends React.Component {
                         <h2>{booksArr.title}</h2>
                         <h2>{booksArr.description}</h2>
                         <h2>{booksArr.price}</h2>
+                        <Button bsStyle = 'primary'>Buy Now</Button>
                     </div>
                     )
         });
         return (
-                <div>
-                    <h1>Hello React</h1> 
-                    {booksList}
-                </div>
-                )
+                <Grid>
+                    <Row style = {{marginTop: '15px'}}>
+                        {booksList}
+                    </Row> 
+                
+                </Grid>
+                            )
+                }
     }
-}
-function mapStateToProps(state) {
-    return {
-        books: state.books.books
+    function mapStateToProps(state) {
+        return {
+            books: state.books.books
+        }
     }
-}
-function mapDispatchToProps (dispatch){
-    return bindActionCreators({getBooks:getBooks},dispatch);
-}
-export default connect(mapStateToProps,mapDispatchToProps)(BookList);
+    function mapDispatchToProps(dispatch) {
+        return bindActionCreators({getBooks: getBooks}, dispatch);
+    }
+    export default connect(mapStateToProps, mapDispatchToProps)(BookList);
