91bde062ac4c14487b2f0a5dec81327ddac6a10d
Viacheslav <sgordievsky@gmail.com>
2018-02-17 21:59:47 +0200

    Section 2 Lecture 19 completed

--- src/components/pages/bookItem.js
+++ src/components/pages/bookItem.js
@@ -1,21 +1,22 @@
-'use strict'
+"use strict"
 import React from 'react';
-import {Row, Col, Well, Button} from 'react-bootstrap';
+import {Row, Col, Well, Button} from
+        'react-bootstrap';
 class BookItem extends React.Component {
     render() {
-        return (
+        return(
                 <Well>
                     <Row>
                         <Col xs={12}>
                         <h6>{this.props.title}</h6>
                         <p>{this.props.description}</p>
-                        <h6>{this.props.price}</h6>
-                        <Button bsStyle = 'primary'>Buy Now</Button>
+                        <h6>usd. {this.props.price}</h6>
+                        <Button bsStyle='primary'>Buy
+                            now</Button>
                         </Col>
                     </Row>
                 </Well>
                 )
     }
 }
-
 export default BookItem;
