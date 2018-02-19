3326bfde315c5a05a9108f9c41eb9d347fbb4b03
Viacheslav_Gordiievs <Viacheslav_Gordiievs@epam.com>
2018-02-19 19:18:53 +0200

    Section 3 lecture 39 completed

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var app = express();
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,11 +29,62 @@ mongoose.connect('mongodb://localhost:27017/bookshop');
 var Books = require('./models/books.js');
 
 //POST BOOKS
-app.post('/books',function(req,res){
+app.post('/books', function (req, res) {
     var book = req.body;
-    
-    Books.create(book,function(err,books){
-        if(err){
+
+    Books.create(book, function (err, books) {
+        if (err) {
+            throw err;
+        }
+        res.json(books);
+    })
+});
+
+//GET BOOKS
+
+app.get('/books', function (req, res) {
+
+
+    Books.find(function (err, books) {
+        if (err) {
+            throw err;
+        }
+        res.json(books);
+    })
+});
+
+//DELETE BOOKS
+
+app.delete('/books/:_id', function (req, res) {
+    var query = {_id: req.params._id}
+
+    Books.remove(query, function (err, books) {
+        if (err) {
+            throw err;
+        }
+        res.json(books);
+    })
+});
+
+//UPDATE BOOKS
+app.put('/books/:_id', function (req, res) {
+    var book = req.body;
+
+    var query = {_id: req.params._id}
+
+    var update = {
+        '$set': {
+            title: book.title,
+            description: book.description,
+            image: book.image,
+            price: book.price
+        }
+    };
+
+    var options = {new:true};
+
+    Books.findOneAndUpdate(query,update,options, function (err, books) {
+        if (err) {
             throw err;
         }
         res.json(books);
@@ -42,25 +93,25 @@ app.post('/books',function(req,res){
 
 //END APIs 
 
-app.get('*',function(req,res){
-    res.sendFile(path.resolve(__dirname,'public','index.html'));
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
