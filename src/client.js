48b9865a3d5d29140db3d770fbb72ef1b72d6209
Viacheslav_Gordiievs <Viacheslav_Gordiievs@epam.com>
2018-02-21 12:16:37 +0200

    Section 3 lecture 55 completed

--- src/client.js
+++ src/client.js
@@ -15,49 +15,16 @@ import reducers from './reducers/index';
 import {addToCart} from './actions/cartActions';
 // STEP 1 create the store
 const middleware = applyMiddleware(thunk,logger);
-const store = createStore(reducers, middleware);
-import BooksList from './components/pages/booksList';
-import Cart from './components/pages/cart';
-import BooksForm from './components/pages/booksForm';
-import Main from './main';
+//WE WILL PASS INITIAL STATE FROM THE SERVER STORE
+const initialState = window.INITIAL_STATE;
+const store = createStore(reducers,initialState, middleware);
+import routes from './routes';
 
 const Routes = (
         <Provider store={store}>
-            <Router history={browserHistory}>
-                <Route path="/" component={Main}>
-                    <IndexRoute
-                        component={BooksList}/>
-                    <Route path="/admin"
-                           component={BooksForm}/>
-                    <Route path="/cart"
-                           component={Cart}/>
-                </Route>
-            </Router>
+    {routes}
         </Provider>
         )
 render(
         Routes, document.getElementById('app')
         );
-
-/// STEP 2 create and dispatch actions
-//store.dispatch(postBooks(
-//        
-//        ))
-//// DELETE a book
-//store.dispatch(deleteBooks(
-//        {id: 1}
-//))
-//// UPDATE a book
-//store.dispatch(updateBooks(
-//        {
-//            id: 2,
-//            title: 'Learn React in 24h'
-//        }
-//))
-//
-//
-////-------------------------------------------
-//
-////-->> CART ACTIONS <<--
-//// ADD TO CART
-//store.dispatch(addToCart([{id: 1}]))
